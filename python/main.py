491134226b87fde1dd2357f450b86cb3b2fb57cf
김회창 <khc9812121@gmail.com>
2024-03-05 17:42:41 +0900

    [PY] init : Fast API 프로젝트 초기화

--- python/main.py
+++ python/main.py
@@ -1,9 +1,19 @@
 from typing import Union
-
 from fastapi import FastAPI
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import uvicorn
 app = FastAPI()
+
+uri = "mongodb+srv://haechang:1234@ibalance.riz077b.mongodb.net/?retryWrites=true&w=majority&appName=ibalance"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 @app.get("/hello")
 def hello_world(user_id : int | None = None):
