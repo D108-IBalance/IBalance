8bac2f69f2cfca28ecbc4b41c793121f2eed4386
kimjuyi <juyi077@naver.com>
2024-03-05 15:42:52 +0900

    [BE] feat : Security 초기 세팅

---
+++ server/src/main/java/com/ssafy/ibalance/auth/config/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.ssafy.ibalance.auth.config;
+
+import com.ssafy.ibalance.auth.type.JwtCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        JwtCode jwtCode = jwtTokenProvider.validateToken(token);
+
+    }
+}
