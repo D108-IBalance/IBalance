e012327c6715240245767bb02d3896d7108d2db8
suehwanBoo <rndjdieo119@naver.com>
2024-03-06 16:38:29 +0900

    [FE] refactor : 로딩페이지 모듈 작성 컨벤션 변경

--- client/iBalance/src/App.jsx
+++ client/iBalance/src/App.jsx
@@ -3,11 +3,13 @@
 
 // 내부모듈
 import  "./App.css";
+import Load from "./modules/Load/Load.jsx";
 
 const App = ()=>{
   return (
     <>
       <h1>iBalance</h1>
+      <Load step={2}/>
     </>
   )
 }
