39b83447333e8dc8f600c5224c376996047b3346
suehwanBoo <rndjdieo119@naver.com>
2024-03-06 16:23:31 +0900

    [FE] feat : 로딩 스피너 기능 완료

--- client/iBalance/src/App.jsx
+++ client/iBalance/src/App.jsx
@@ -1,6 +1,10 @@
-function App() {
+/* eslint-disable */
 
 
+// 내부모듈
+import  "./App.css";
+
+const App = ()=>{
   return (
     <>
       <h1>iBalance</h1>
---
+++ client/iBalance/src/modules/Load/Load.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable */
+
+// 외부 모듈
+import { useRef } from "react";
+import Lottie from 'react-lottie';
+import Icon from "../../assets/load/json/Lottie-load.json";
+
+// 내부 모듈
+import './Load.css';
+
+/* 
+    props값 step을 통해 상태 업데이트 가능
+    key(step)
+        - 0 : default(none)
+        - 1 : (off -> on)
+        - 2 : (on/none->off)
+*/
+const Load = (props)=>{ 
+    let { step } = props;
+    let isShow = false;
+    let fade = "";
+
+    switch(step) {
+        case 1: // on -> off 애니메이션 실행
+            isShow = true;
+            fade = "fadeOut";
+            break;
+        case 2: // off -> on
+            isShow = true;
+            fade = "";
+            break;
+        default: // 디폴트값 명시적 작성
+            isShow = false;
+    }
+
+    return(
+        <>
+        {
+            isShow?( //isShow값에 따른 랜더 결정 default 제외 모두 랜더
+                <div id="spinner">
+                    <Spinner fade={fade}></Spinner>
+                </div>
+            ):(
+                <></>
+            )
+        }
+        </>
+    )
+}
+
+
+const Spinner = (props)=>{
+    const defaultOptions = { // lottie Options
+        loop: true,
+        autoplay: true,
+        animationData: Icon,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        },
+      };
+
+    let {fade} = props // 애니메이션 클래스
+    
+    return(
+        <div className={`container ${fade}`} >
+            <Lottie options={defaultOptions} height={100} width={100}/>
+            <span className="loadText">Loading...</span>
+        </div>
+    )
+}
+
+export default Load
