86c55dd11b749f5553f3a8019a38f873ff51cd4d
suehwanBoo <rndjdieo119@naver.com>
2024-03-06 16:39:37 +0900

    [FE] refactor : CSS 모듈 컨밴션 변경

--- client/iBalance/src/App.jsx
+++ client/iBalance/src/App.jsx
@@ -3,13 +3,12 @@
 
 // 내부모듈
 import  "./App.css";
-import Load from "./modules/Load/Load.jsx";
+
 
 const App = ()=>{
   return (
     <>
       <h1>iBalance</h1>
-      <Load step={2}/>
     </>
   )
 }
