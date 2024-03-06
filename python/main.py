d2049c612890769d9535967c35e5068dae351e21
김회창 <khc9812121@gmail.com>
2024-03-06 10:30:08 +0900

    [PY] chore : 몽고DB 접속정보 환경변수 주입 방식으로 변경

--- python/main.py
+++ python/main.py
@@ -3,12 +3,18 @@ from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import uvicorn
+from pydantic_settings import BaseSettings
+class Settings(BaseSettings):
+    MONGO_USERNAME : str
+    MONGO_PASSWORD : str
+    MONGO_HOST: str
+
+
+settings = Settings()
 app = FastAPI()
 
-uri = "mongodb+srv://haechang:1234@ibalance.riz077b.mongodb.net/?retryWrites=true&w=majority&appName=ibalance"
-# Create a new client and connect to the server
+uri = f'mongodb+srv://{settings.MONGO_USERNAME}:{settings.MONGO_PASSWORD}{settings.MONGO_HOST}'
 client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -19,5 +25,11 @@ except Exception as e:
 def hello_world(user_id : int | None = None):
     return {"Hello": "World"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=5050, log_level="debug")
+@app.get("/mongo/info")
+async def get_info():
+    return {
+        "mongo_username" : settings.MONGO_USERNAME,
+        "mongo_password" : settings.MONGO_PASSWORD,
+        "mongo_host" : settings.MONGO_HOST
+    }
+
