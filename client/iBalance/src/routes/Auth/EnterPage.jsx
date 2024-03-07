fe96c96886e4f98d6a9bb90fb575660ae895e2b7
hyunjihye <hhh259@naver.com>
2024-03-07 10:51:26 +0900

    [FE] refactor : loginform 구조 수정

---
+++ client/iBalance/src/routes/Auth/EnterPage.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+// 외부 모듈
+import {Container,  Row} from 'react-bootstrap';
+import { Link, useNavigate, Outlet } from 'react-router-dom';
+
+// 내부 모듈
+import classes from '../../assets/auth/EnterPage.module.css'
+
+const EnterPage = ()=>{
+    return(
+        <>
+            <div className={classes.loginBack}>
+                <Outlet></Outlet>
+            </div>
+        </>
+    )
+
+}
+
+
+export default EnterPage
