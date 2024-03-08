ed6dba0b0d82f35ea329716118862c0bc560a281
남동우 <ndw8200@naver.com>
2024-03-08 13:50:50 +0900

    [BE] refactor : Kakao 소셜로그인 을 타겟팅한 Custom Exception 을 Social Login 통합으로 처리하기 위해 리팩토링

--- server/src/main/java/com/ssafy/ibalance/member/exception/KakaoInfoIsNullException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.ibalance.member.exception;
-
-public class KakaoInfoIsNullException extends RuntimeException {
-    public KakaoInfoIsNullException(String message) {
-        super(message);
-    }
-}
