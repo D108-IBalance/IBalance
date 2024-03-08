d77dea34fcceb55ab330787fda145d4ffac16b93
남동우 <ndw8200@naver.com>
2024-03-08 13:59:49 +0900

    [BE] remove : 중복으로 JwtTokenResponse 만들어서 삭제

--- server/src/main/java/com/ssafy/ibalance/member/dto/response/JwtTokenResponse.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.ibalance.member.dto.response;
-
-import com.ssafy.ibalance.member.type.OAuthProvider;
-import lombok.Builder;
-
-@Builder
-public record JwtTokenResponse(String accessToken, String tokenType, OAuthProvider provider) {
-}
