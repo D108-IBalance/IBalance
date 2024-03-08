2a0bdc905753fac26ac21cba08005949602c6d86
hyunjihye <hhh259@naver.com>
2024-03-08 14:12:02 +0900

    [FE] feat :  메인페이지 오늘의 식단표 view 구혀

--- client/iBalance/src/routes/Auth/LoginForm.jsx
+++ client/iBalance/src/routes/Auth/LoginForm.jsx
@@ -1,36 +1,37 @@
 /* eslint-disable */
 
 import { useNavigate } from "react-router-dom";
-import classes from './LoginForm.module.css'
-import {Container,  Row} from 'react-bootstrap';
+import classes from "./LoginForm.module.css";
+import { Container, Row } from "react-bootstrap";
 import { useEffect } from "react";
 
-const LoginForm = ()=>{
+const LoginForm = () => {
+  const SocialLogin = (num) => {
+    let server = "";
+    if (num == 0) {
+      // server = process.env.GOOGLE_AUTH_URL;
+      server = home;
+    } else if (num == 1) {
+      // server = process.env.KAKAO_AUTH_URL;
+      server = home;
+    } else {
+      // server = process.env.NAVER_AUTH_URL;
+      server = home;
+    }
+    window.location.href = server;
+  };
 
-    const SocialLogin = (num) =>{
-        let server = '';
-        if ( num == 0 ){
-            server = process.env.GOOGLE_AUTH_URL;
-        }else if ( num == 1 ){
-            server = process.env.KAKAO_AUTH_URL;
-        }else {
-            server = process.env.NAVER_AUTH_URL;
-        }
-        window.location.href = server;
+  return (
+    <div className={classes.loginFormbox}>
+      <Container className={classes.loginForm}>
+        <Row className={classes.loginLogo}></Row>
 
-    };
+        <Row className={classes.loginGoogle} onClick={SocialLogin(0)}></Row>
+        <Row className={classes.loginKakao} onClick={SocialLogin(1)}></Row>
+        <Row className={classes.loginNaver} onClick={SocialLogin(2)}></Row>
+      </Container>
+    </div>
+  );
+};
 
-    return(
-        <div className={classes.loginFormbox}>
-            <Container className={classes.loginForm}>
-                <Row className={classes.loginLogo}></Row>
-
-                <Row className={classes.loginGoogle} onClick={SocialLogin(0)}></Row>
-                <Row className={classes.loginKakao} onClick={SocialLogin(1)}></Row>
-                <Row className={classes.loginNaver}onClick={SocialLogin(2)}></Row>
-            </Container>
-        </div>
-    )
-}
-
-export default LoginForm
+export default LoginForm;
