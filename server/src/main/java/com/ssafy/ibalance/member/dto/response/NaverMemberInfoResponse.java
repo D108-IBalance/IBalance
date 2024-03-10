435a20102bd88e4bd1dcb5216fe0a77a20e70536
남동우 <ndw8200@naver.com>
2024-03-10 20:36:01 +0900

    [BE] feat : 네이버 소셜 로그인 구현

---
+++ server/src/main/java/com/ssafy/ibalance/member/dto/response/NaverMemberInfoResponse.java
@@ -0,0 +1,24 @@
+package com.ssafy.ibalance.member.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.ibalance.member.dto.OAuthMemberInfo;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+public class NaverMemberInfoResponse implements OAuthMemberInfo {
+
+    @JsonProperty("resultcode")
+    private String resultCode;
+
+    private String message;
+
+    private NaverDetailResponse response;
+    @Override
+    public String code() {
+        return response.id();
+    }
+}
