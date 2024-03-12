70ab679e84ed80a4f993f6c34714f2ca75c39460
kimjuyi <juyi077@naver.com>
2024-03-12 11:18:23 +0900

    [BE] refactor : JWT Token 검증 후 회원 정보 SecurityContextHolder에 저장

--- server/src/main/java/com/ssafy/ibalance/member/service/CustomUserDetailsService.java
+++ server/src/main/java/com/ssafy/ibalance/member/service/CustomUserDetailsService.java
@@ -1,15 +1,30 @@
 package com.ssafy.ibalance.member.service;
 
+import com.ssafy.ibalance.member.exception.OAuthInfoNullException;
+import com.ssafy.ibalance.member.repository.MemberRepository;
+import com.ssafy.ibalance.member.type.OAuthProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        String[] split = username.split(":");
+
+        OAuthProvider provider = OAuthProvider.getOAuthProvider(split[0]);
+        String code = split[1];
+
+        // TODO : JWT payload 데이터로 유저 가져오기 (DB 접근 x)
+
+        return memberRepository.findByCodeAndProvider(code, provider)
+                .orElseThrow(()-> new OAuthInfoNullException("해당하는 회원이 없습니다."));
     }
 }
