14a50dab47f2eb4705ad4ab99d7bc87451adeb4b
김주이 <juyi077@naver.com>
2024-03-13 14:30:59 +0900

    [BE] feat : main 페이지에서 오늘의 식단 정보 조회 기능 구현

--- server/src/main/java/com/ssafy/ibalance/diet/dto/response/DietByDateResponse.java
+++ server/src/main/java/com/ssafy/ibalance/diet/dto/response/DietByDateResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.ibalance.diet.dto.response;
 
+import com.ssafy.ibalance.diet.entity.DietMenu;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,5 +13,5 @@ public class DietByDateResponse {
     private Long dietId;
     private LocalDate dietDate;
     private Integer sequence;
-    private List<DietMenuResponse> dietMenuList;
+    private List<DietMenu> dietMenuList;
 }
