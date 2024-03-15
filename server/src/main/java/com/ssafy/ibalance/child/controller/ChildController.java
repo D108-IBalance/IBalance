dfa934840ab16a13436dd69da83cfe483332a3d6
박성진 <p7364009@gmail.com>
2024-03-15 14:49:45 +0900

    [BE] feat : 이미 추천받은 식단 조회 기능 완료

--- server/src/main/java/com/ssafy/ibalance/child/controller/ChildController.java
+++ server/src/main/java/com/ssafy/ibalance/child/controller/ChildController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/child")
+@RequestMapping("child")
 @RequiredArgsConstructor
 public class ChildController {
 
--- server/src/main/java/com/ssafy/ibalance/child/service/ChildService.java
+++ server/src/main/java/com/ssafy/ibalance/child/service/ChildService.java
@@ -56,7 +56,7 @@ public class ChildService {
 
     private Child saveChild(RegistChildRequest registChildRequest, Member member) {
 
-        return Child.ConvertDtoToEntity(registChildRequest, member);
+        return childRepository.save(Child.ConvertDtoToEntity(registChildRequest, member));
     }
 
     private void saveGrowth(Child child) {
