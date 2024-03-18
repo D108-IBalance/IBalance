d8b326e719de8e55b14eebe38a25904f5c26ff01
김회창 <khc9812121@gmail.com>
2024-03-18 22:31:10 +0900

    [test] ubcf 알고리즘을 식단데이터에 맞게 커스텀

--- python/main.py
+++ python/main.py
@@ -1,137 +1,45 @@
-from typing import Union
 from fastapi import FastAPI
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-import uvicorn
-# from recommend import recommend_first_init
+from recommend import getRecomm
 from pydantic_settings import BaseSettings
-from UBCFPractice import read_ratings, read_names, set_user_id, getRecomm
-import mysql.connector
-from make_random_rating import make
-import csv
+from dbUtil.mongodb_api import mongodb_connect
+from dbUtil.mysql_api import mysql_connect
+from request.request_dto import ChildInfo
+from response.response_dto import Diet
 
-
+"""
+@Author : 김회창
+"""
 
 class Settings(BaseSettings):
-    MONGO_HOST: str
-    MYSQL_HOST: str
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
-    MYSQL_DATABASE: str
+    """
+    pydantic 라이브러리를 이용하여 환경변수를 실행옵션으로 주입받는 클래스
+    """
+    MONGO_HOST: str # 몽고DB 호스트 주소
+    MYSQL_HOST: str # MySQL 호스트 주소
+    MYSQL_USER: str # MySQL 접속 유저 명
+    MYSQL_PASSWORD: str # MySQL 패스워드
+    MYSQL_DATABASE: str # MySQL 접속 대상 스키마
 
-settings = Settings()
 
-app = FastAPI()
-# ratings = []
+
+
+settings = Settings() # 클래스 객체 생성
+
+app = FastAPI() # Fast API 앱 객체 생성
+
 uri = f'{settings.MONGO_HOST}'
-client = MongoClient(uri, server_api=ServerApi('1'))
 
-mydb = mysql.connector.connect(
-    host=settings.MYSQL_HOST,
-    user=settings.MYSQL_USER,
-    password=settings.MYSQL_PASSWORD,
-    database=settings.MYSQL_DATABASE
-)
-
-if mydb.is_connected():
-    print("mysql is connected!!!")
+mongodb_connect(uri) # 몽고DB 연결
+mysql_connect(settings.MYSQL_HOST, settings.MYSQL_USER, settings.MYSQL_PASSWORD, settings.MYSQL_DATABASE) # MySQL 연결
 
 
+@app.get("/recomm/init")
+def init_recommend(request: ChildInfo):
+    """
+    :param: ChildInfo
+    :ChildInfo: childId: 아이 고유번호, allergyList: 그 아이가 갖고있는 알러지, cacheList: 그날 새로고침 되었던 menu_id
+    :return: List of Dictionary
+    :Diet:
+    """
+    return getRecomm((int(request.childId)),0)
 
-def test():
-    cur = mydb.cursor()
-    sql = '''SELECT * FROM rating WHERE 1=1'''
-    cur.execute(sql)
-    result = cur.fetchall()
-    for res in result:
-        print(res)
-# mysql 접속 테스트, 아래 주석 제거할 것
-# test()
-
-# db = client.get_database("test")
-# db.list_collections()
-# menu_cols = db.get_collection("menu")
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-@app.get("/hello")
-def hello_world(user_id : int | None = None):
-    return {"Hello": "World"}
-
-# @app.get("/make")
-# def make_random_rating():
-#     make(client, mydb)
-#     return{}
-# def _load_movies(csv_path : str):
-#     f = open(csv_path, 'r', encoding='utf-8')
-#     lines = csv.reader(f)
-#     for line in lines:
-#         if line[0] == 'userId':
-#             continue
-#         movie_id = line[0]
-#         title = line[1]
-#         genre = line[2]
-#         movies.append(
-#             {
-#                 'movie_id' : movie_id,
-#                 'title' : title,
-#                 'genre' : genre
-#             }
-#         )
-#     f.close()
-#
-# def _load_ratings(csv_path : str):
-#     f = open(csv_path, 'r', encoding='utf-8')
-#     lines = csv.reader(f)
-#     for line in lines:
-#         if line[0] == 'movieId':
-#             continue
-#         user_id = line[0]
-#         movie_id = line[1]
-#         rating = line[2]
-#         ratings.append({
-#             'user_id' : user_id,
-#             'movie_id' : movie_id,
-#             'rating' : rating
-#         })
-#     f.close()
-# @app.get("/movie/push")
-# def movie_insert():
-#     database = client["test"]
-#     movies_collection = database["movies"]
-#     _load_movies("movies.csv")
-#     movies_collection.insert_many(movies)
-#
-# @app.get("/rating/push")
-# def rating_insert():
-#     database = client["test"]
-#     ratings_collection = database["ratings"]
-#     ratings_collection.drop()
-#     _load_ratings("ratings.csv")
-#     ratings_collection.insert_many(ratings)
-
-@app.get("/recomm/{child_id}")
-def init_recommend(user_id : str):
-    print("req user_id : ")
-    print(int(user_id))
-    # recommend_first_init(mydb, client, (int(user_id)))
-    # return getRecomm(client, (int(user_id)))
-
-@app.get("/mongo/info")
-async def get_info():
-    return {
-        "mongo_host" : settings.MONGO_HOST
-    }
-
-
-@app.get("/mongo/menu")
-def get_mongo_menu():
-   test_db = client["test"]
-   menu_cols = test_db["menu"]
-   result = menu_cols.find()
-   for res in result:
-       print(res)
--- python/recommend.py
+++ python/recommend.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
-import csv
+from dbUtil.mongodb_api import find_by_object_id
+from dbUtil.mysql_api import find_all
 
 # 유저의 이웃들의 수
 N_NEIGHBORS = 10
@@ -8,7 +8,6 @@ N_NEIGHBORS = 10
 # 추천 데이터 개수
 N_RECOMMENDATIONS = 5
 ratings = []
-movie_names = []
 
 user_id = ""
 
@@ -20,91 +19,28 @@ def _init():
     movie_names = []
     user_id = None
 
-def read_ratings(mongo_ratings):
+def read_ratings():
     """
     레이팅 데이터 읽기
     :param path: 레이팅 데이터 path
-    :return: list of tuples : (user_id, movie_id, rating)
+    :return: list of tuples : (user_id, menu_id, rating)
     """
     global ratings
-    for rating in mongo_ratings:
+    mysql_result = find_all("menu_rating_cp")
+    for rating in mysql_result:
         ratings.append((
-            int(rating["user_id"]),
-            rating["movie_id"],
-            rating["rating"]
+            int(rating[1]),
+            rating[2],
+            rating[3]
         ))
 
-
-
-def read_names(mongo_movies):
-    """
-    영화 id와 실제 영화 id에 대응되는 영화 name
-    :param path: 영화 데이터 경로
-    :return: dictionary key : movie_id, value : title
-    """
-    data = {}
-    for movie in mongo_movies:
-        movie_id = int(movie["movie_id"])
-        title = movie["title"]
-        data[movie_id] = title
-    global movie_names
-    movie_names = data
 
 def set_user_id(id):
     global user_id
     user_id = id
 
-# ratings = read_ratings(MOVIE_RATINGS_PATH)
-
-
-# pd.DataFrame()
-
-# data 확인
-# print(ratings.head())
-
-# 레이팅 매겨진 영화의 개수
-# print(ratings.movie.nunique())
-
-# 레이팅을 매긴 유저의 수
-# print(ratings.user.nunique())
-
-# 테스트 목적의 데이터 선정하기
-# sample = ratings.sample(random_state=42)
-# item_id = sample.movie.values[0]
-
-# 테스트 목적의 user_id와 item_id 출력
-# print(user_id, item_id)
-
-# ratings_raw = ratings.copy()
-# ratings = ratings.pivot(index='user', columns='movie', values='rating')
-# ratings = ratings.astype(float)
-
-# print(ratings.head())
-
-# Movielens 데이터셋은 소윔라해 너무 긴 형식이다.
-# 따라서 필요한 데이터만 빼내어 사용할 필요가 있으므로 정제를 한다.
-
-# print(ratings.loc[user_id, :].sample(10))
 
 # 유저들 사이의 유사도 측정하기
-"""
-레이팅 벡터는 유저들 사이의 유사한지 혹은 유사하지 않은지를 측정하는데 사용된다.
-두명의 유저가 같은 영화에 대해 같은 레이팅을 부여했다고 했을 때,
-이 두 유저들은 그들의 영화 입맛을 기반으로 유사하다고 볼 수 있다.
-그 반대로, 두 유저가 매우 다르게 같은 영화에 대해 평가했다면, 
-그 유저들은 해당 영화에 대해 매우 다른 입맛을 가지고 있다.
-
-유저 기반 협업 필터링은 다른 누군가에게 영화를 추천하기 위해 
-매우 유사한 입맛을 가진 유저를 찾는데 초점을 맞춘다.
-
-우리는 Pearson 상관관계를 사용하여 두 레이팅 벡터 사이에서 유사도를 측정한다.
-Pearson 상관관계는 -1~+1 사이의 값을 가진다. 
-0은 상관관계가 없다는것을 의미한다.
-1은 매우 유사하단것이고, -1은 매우 유사하지 않다는 것이다.
-
-데이터셋에서 어떤 사람과 모든 다른 사람들과의 유사도를 계산하자.
-"""
-
 
 def pearson_similarity(v1, v2):
     """
@@ -115,9 +51,6 @@ def pearson_similarity(v1, v2):
     pearson = v1.corr(v2)
     return pearson
 
-
-# pearson 유사도 테스트
-# print(pearson_similarity(pd.Series([1,2,3,4,5, np.NaN]), pd.Series([2,1,4,5,np.NaN, 1])))
 
 def compute_similarity(user_id, ratings_matrix):
     """
@@ -144,16 +77,6 @@ def compute_similarity(user_id, ratings_matrix):
     return similarities
 
 
-# similarities = compute_similarity(user_id, ratings)
-# print(similarities.head())
-
-"""
-사용자 유사도는 어떤 유저가 레이팅을 매기지않은 영화에 대한 레이팅을 예측한다.
-레이팅 예측을 하는 방법은 아래와 같다.
-    1. 주어진 영화를 평가한 사용자와 가장 유사한 N명의 이웃을 찾는다.
-    2. 그 유저의 N명의 이웃에 대한 영화 레이팅의 평균을 낸다.
-"""
-
 
 def predict_rating(item_id, ratings, similarities, N=10):
     # print("들어 오는 데이터 : "+str(item_id))
@@ -179,111 +102,65 @@ def predict_rating(item_id, ratings, similarities, N=10):
     ratings_for_item = ratings.loc[N_most_similar_users.index, item_id]
     return ratings_for_item.mean()
 
-"""
-이제 어떤 유저가 안본 영화에 대하여 유사도를 매기기 위해
-해당 유저와 유사한 유저들 중 해당 영화에 레이팅을 매긴 유저들만을 간추리고
-그 유저들이 매긴 해당 영화의 레이팅값 평균을 가지고 예측하였다.
-
-어떤 유저가 레이팅을 매기지않은 영화를 찾는다.
-이러한 레이팅이 매겨지지않은 영화들에 대하여 사용자가 부여할 레이팅 값을 예측합니다.
-가장 높은 레이팅 값들을 가진 N개의 영화를 찾는다.
-"""
-
-
-def recommend(user_id, ratings, movie_names, n_neighbors=10, n_recomm=5):
+def recommend(n_neighbors=10, n_recomm=5):
     """
     레이팅 데이타를 기반으로 유저에게 N개의 영화를 추천해준다.
-     1. 해당 유저의 영화 레이팅값들을 들고온다.
-     2. 유저가 레이팅을 부여하지 않은 영화값들을 들고온다.
+     1. 해당 유저의 메뉴 레이팅값들을 들고온다.
+     2. 유저가 레이팅을 부여하지 않은 메뉴값들을 들고온다.
      3. 유저와 다른 유저들 사이의 유사도를 계산한다.
-     4. 다른 유저와의 유사도를 기반으로 하여 해당 유저에 대한 영화 레이팅 예측값을 생성한다.
-     5. 예측값 중 가장 높은 N개의 영화를 찾는다.
+     4. 다른 유저와의 유사도를 기반으로 하여 해당 유저에 대한 메뉴 레이팅 예측값을 생성한다.
+     5. 예측값 중 가장 높은 N개의 메뉴를 찾는다.
     :param user_id: int, 추천받는 사람의 user_id
-    :param ratings: pd.DataFrame, user_id와 movie rating값
-    :param movie_names: dict key : movie_id, value : movie_name
+    :param ratings: pd.DataFrame, user_id와 menu rating값
+    :param movie_names: dict key : movie_id, value : menu_name
     :param n_neighbors: int, 레이팅 예측값을 생성하기 위해 사용되는 이웃의 수
     :param n_recomm: int, 추천받을 영화의 개수
     :return: pd.DataFrame with [movie_id, rating, movie_name]
     """
     # Data 형식 바꾸기
-    ratings = pd.DataFrame(data=ratings, columns=['user', 'movie', 'rating'])
-    print("pd ratings : ")
-    print(ratings)
-
-    ratings = ratings.pivot(index='user', columns='movie', values='rating')
-    ratings = ratings.astype(float)
+    global ratings
+    global user_id
+    rating_pd = pd.DataFrame(data=ratings, columns=['user_id', 'menu_id', 'rating'])
+    rating_pd = rating_pd.pivot(index='user_id', columns='menu_id', values='rating')
 
     # 해당 유저의 레이팅들을 가져온다.
-    ratings_users = ratings.loc[user_id, :]
+    ratings_users = rating_pd.loc[user_id, :]
 
-    all_items = ratings.loc[user_id, :]
-    print(f'user_id : {user_id} 가 레이팅을 매긴 모든 영화')
+    all_items = rating_pd.loc[user_id, :]
 
     unrated_items = all_items.loc[all_items.isnull()]
-
     # item_id값들에서 Series 값들로 인덱싱을 바꾼다.
     unrated_items = unrated_items.index.to_series(name='item_ids').reset_index(drop=True)
-    print('User {} has {} unrated items'.format(user_id, len(unrated_items)))
-    # print("unrated_ items : ")
-    print(unrated_items)
-    # unrated_items.map(lambda x: print(x in movie_names))
-    # print(movie_names[1818])
     # 유사도를 계산한다.
-    sim = compute_similarity(user_id, ratings)
+    sim = compute_similarity(user_id, rating_pd)
     #unrated_items.apply(lambda d: print("d : {} in movie_names : {}, type {}".format(int(d), int(d) in movie_names, type(d))))
     # 해당 유저의 유사도 데이터를 기반으로 보지않은 아이템들에 대한 예측값을 생성한다.
-    print(sim.head())
-    predictions = unrated_items.apply(lambda d: predict_rating(d, ratings, sim, N=n_neighbors))
-    print(predictions.head())
+    predictions = unrated_items.apply(lambda d: predict_rating(d, rating_pd, sim, N=n_neighbors))
     predictions = predictions.sort_values(ascending=False)
     # top N개의 아이템들을 추천한다.
     recomms = predictions.head(n_recomm)
-    print("recomm : ")
-    print(recomms)
     # 결과값을 형식에 맞춘다.
     recomms = recomms.to_frame(name='predicted_rating')
     recomms = recomms.rename_axis('u_index')
     recomms = recomms.reset_index()
-    recomms['movie_id'] = recomms.u_index.apply(lambda d: unrated_items[int(d)])
-    print(recomms['movie_id'])
-    recomms['name'] = recomms.movie_id.apply(lambda d: movie_names[int(d)])
-    recomms = recomms.reset_index()
-
-    print("recomm movie_id")
-    print(recomms['movie_id'])
-    print("recomm movie_names")
-    print(recomms['name'])
-    data = []
-    for i in range(0, n_recomm):
-        frag = {}
-        frag['movie_id'] = recomms['movie_id'][i]
-        frag['movie_name'] = recomms['name'][i]
-        data.append(frag)
-    return data
+    recomms['menu_id'] = recomms.u_index.apply(lambda d: unrated_items[int(d)])
+    result = []
+    recomms.menu_id.apply(lambda id: result.append(find_by_object_id("menu",id)))
+    return result
 
 # recommends = recommend(user_id, ratings, movie_names, n_neighbors=N_NEIGHBORS, n_recomm=N_RECOMMENDATIONS)
+# 국 하나 추천
+# 밥 하나 추천
+# 반찬 두번 추천
+# 무조건 밥 국 반찬 순서로 일단 진행
 
-def getRecomm(client, id : int):
+
+def getRecomm(id, option):
     _init()
     global user_id
     global ratings
     global movie_names
     set_user_id(id)
-    test_db = client.get_database("test")
-    movie_cols = test_db.get_collection("movies")
-    rating_cols = test_db.get_collection("ratings")
-    mongo_ratings = rating_cols.find()
-    mongo_movies = movie_cols.find()
-    read_ratings(mongo_ratings)
-    read_names(mongo_movies)
-    recommends = recommend(user_id, ratings, movie_names, n_neighbors=N_NEIGHBORS, n_recomm=N_RECOMMENDATIONS)
+    read_ratings()
+    recommends = recommend(n_neighbors=N_NEIGHBORS, n_recomm=N_RECOMMENDATIONS)
     return recommends
-
-## must copy    
-
-# def recommend_first_init(mysql_client, mongo_client, req_user_id):
-#     get_all_user_data()
-#     return
-#
-#
-# recommend_first_init()
