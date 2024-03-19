652684d40de70e25302f9ac6f96dc1e8dea08ecd
hyunjihye <hhh259@naver.com>
2024-03-19 17:52:34 +0900

    [FE] feat : access token store 저장

--- client/iBalance/src/routes/Auth/SocialLogin.jsx
+++ client/iBalance/src/routes/Auth/SocialLogin.jsx
@@ -1,17 +1,19 @@
 // 외부 모듈
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 // 내부 모듈
 import Loading from "../../modules/Load/Load.jsx";
 import { getToken } from "./ServerConnect.js";
+import { setToken } from "../../store.js";
 
 const SocialLogin = () => {
   // redierct URL 파싱
+  const dispatch = useDispatch();
   const params = new URL(window.location.href).searchParams;
   const code = params.get("code");
   const { provider } = useParams();
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,6 +22,7 @@ const SocialLogin = () => {
         const value = await getToken(code, provider);
         console.log(value.data);
         const accessToken = value.data.data.accessToken;
+        dispatch(setToken(accessToken));
         console.log("accessToken:", accessToken);
         navigate("/home");
       } catch (err) {
@@ -27,7 +30,7 @@ const SocialLogin = () => {
       }
     };
     login();
-  }, [code, navigate, provider]); // 의존성 배열 축
+  }, [code, dispatch, navigate, provider]); // 의존성 배열 축
 
   return <Loading step={2} />;
 };
