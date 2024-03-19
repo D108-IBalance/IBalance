95dc83084aace7a8ceb63523ae2e7acb533de9c3
suehwanBoo <rndjdieo119@naver.com>
2024-03-19 10:23:15 +0900

    [FE] refactor : 홈페이지 반응형 개선 및 캘린더 성능 개선

--- client/iBalance/src/routes/Home/HomePage.jsx
+++ client/iBalance/src/routes/Home/HomePage.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable */
 
 // 외부 모듈
-import NavbarModule from "../../modules/Navbar/NavbarModule";
 
 // 내부 모듈
+
+import NavbarModule from "../../modules/Navbar/NavbarModule";
 import classes from "./HomePage.module.css";
-import profileImgfile from "../../assets/auth/img/default_profile1.png";
+import ProfileView from "./ProfileView";
+import TodayDiet from "./TodayDiet";
 import { useEffect, useState } from "react";
 import GrowthCard from "./GrowthCard";
 
@@ -14,72 +16,15 @@ const HomePage = () => {
     <>
       <div className={classes.gridSet}>
         <NavbarModule isClick={0}></NavbarModule>
-        <div style={{ width: "100%" }}>
+        <div className={classes.container} style={{ width: "100%" }}>
           <ProfileView />
           <div className={classes.homeContentBack}>
-            <TodayDiet />
+            <TodayDiet></TodayDiet>
             <GrowthCard />
           </div>
         </div>
       </div>
     </>
-  );
-};
-
-const ProfileView = () => {
-  return (
-    <div className={classes.profileBox}>
-      <div className={classes.headerBox}>
-        <div className={classes.tempIcon}></div>
-        <div className={classes.homeLogo}></div>
-        <div className={classes.toggleBtn}></div>
-      </div>
-      <div className={classes.profileContentBox}>
-        <div className={classes.profileImg}>
-          <img src={profileImgfile} alt="" />
-        </div>
-        <div className={classes.profileContent}>
-          <p className={classes.profileName}>
-            박서준<span>(만 11세)</span>
-          </p>
-          <div className={classes.growthBack}>
-            <div className={classes.growthLine}></div>
-            <div className={classes.profiletextBox}>
-              <div>
-                <p className={classes.profiletext}>신장</p>
-                <p className={classes.profiledata}>135cm</p>
-                <div className={classes.normal}></div>
-              </div>
-              <div>
-                <p className={classes.profiletext}>몸무게</p>
-                <p className={classes.profiledata}>30kg</p>
-                <div className={classes.normal}></div>
-              </div>
-              <div>
-                <p className={classes.profiletext}>BMI</p>
-                <p className={classes.profiledata}>20.71</p>
-                <div className={classes.normal}></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const TodayDiet = () => {
-  return (
-    <div className={classes.dietBox}>
-      <div className={classes.dietTitle}>
-        <div className={classes.dietIcon}></div>
-        <span>오늘의 식단표</span>
-      </div>
-      <div className={classes.todayDietBox}>
-        <div className={classes.emptyIcon}></div>
-        <p>오늘의 식단을 추천받아보세요.</p>
-      </div>
-    </div>
   );
 };
 
