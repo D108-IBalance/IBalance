3757b445a139939419731c61894bea5184529d9c
김주이 <juyi077@naver.com>
2024-03-19 17:30:01 +0900

    [BE] feat : 식단이 있는 날짜, 리뷰 작성 여부 조회 기능 구현

---
+++ server/src/main/java/com/ssafy/ibalance/diary/controller/DiaryController.java
@@ -0,0 +1,23 @@
+package com.ssafy.ibalance.diary.controller;
+
+import com.ssafy.ibalance.diary.dto.response.CalendarResponse;
+import com.ssafy.ibalance.diary.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/diary")
+public class DiaryController {
+
+    private final DiaryService diaryService;
+
+    @GetMapping("/calendar/{childId}")
+    public List<CalendarResponse> getCalenderList(@PathVariable Integer childId,
+                                                  @RequestParam int year, @RequestParam int month) {
+        return diaryService.getCalendarList(childId, year, month);
+    }
+
+}
