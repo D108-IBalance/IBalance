9b05c19d42cc17e5d97de36d772d93ef480ec766
남동우 <ndw8200@naver.com>
2024-03-22 11:38:29 +0900

    [BE] chore : 기존에 맞지 않던 코드 컨벤션 수정

--- server/src/main/java/com/ssafy/ibalance/member/type/OAuthProvider.java
+++ server/src/main/java/com/ssafy/ibalance/member/type/OAuthProvider.java
@@ -5,11 +5,10 @@ import com.ssafy.ibalance.member.exception.ProviderNotSupportedException;
 public enum OAuthProvider {
     GOOGLE, KAKAO, NAVER;
 
-    public static OAuthProvider getOAuthProvider(String inputProvider){
-
+    public static OAuthProvider getOAuthProvider(String inputProvider) {
         String convertProvider = inputProvider.toUpperCase();
 
-        return switch (convertProvider){
+        return switch (convertProvider) {
             case "KAKAO" -> KAKAO;
             case "NAVER" -> NAVER;
             case "GOOGLE" -> GOOGLE;
