de69c640971a54e00cf02ddc868d88396064cb87
남동우 <ndw8200@naver.com>
2024-03-22 09:45:49 +0900

    [BE] feat : 로그인/로그아웃 시 리프레시 토큰을 Redis 에 저장하는 기능 추가

---
+++ server/src/main/java/com/ssafy/ibalance/member/repository/RefreshTokenRedisRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.ibalance.member.repository;
+
+import com.ssafy.ibalance.member.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+    boolean deleteByRefreshToken(String refreshToken);
+}
