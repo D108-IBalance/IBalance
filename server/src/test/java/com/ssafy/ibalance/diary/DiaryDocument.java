0b085b497ce7387893163112918b69396ac35839
김주이 <juyi077@naver.com>
2024-03-26 09:04:37 +0900

    [BE] test : 일기장 페이지에서 선택한 날짜의 식단 조회 테스트 코드 구현

--- server/src/test/java/com/ssafy/ibalance/diary/DiaryDocument.java
+++ server/src/test/java/com/ssafy/ibalance/diary/DiaryDocument.java
@@ -22,4 +22,14 @@ public class DiaryDocument {
             fieldWithPath("data[].dietDate").type(JsonFieldType.STRING).description("식단이 존재하는 날짜").optional(),
             fieldWithPath("data[].allReviewed").type(JsonFieldType.BOOLEAN).description("모든 식단의 리뷰 여부").optional()
     );
+
+    public static final Snippet getDietByDateResponseField = responseFields(
+            fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+            fieldWithPath("data[].dietId").type(JsonFieldType.NUMBER).description("식단 아이디").optional(),
+            fieldWithPath("data[].dietDate").type(JsonFieldType.STRING).description("식단 날짜").optional(),
+            fieldWithPath("data[].sequence").type(JsonFieldType.NUMBER).description("식단 순서 (아침 : 1, 점심 : 2, 저녁 : 3, 순서 없음 : 4)").optional(),
+            fieldWithPath("data[].menuList[].menuId").type(JsonFieldType.NUMBER).description("메뉴 아이디"),
+            fieldWithPath("data[].menuList[].menuName").type(JsonFieldType.STRING).description("메뉴명"),
+            fieldWithPath("data[].menuList[].menuType").type(JsonFieldType.STRING).description("메뉴 타입 (밥 : RICE, 국 : SOUP, 반찬 : SIDE)")
+    );
 }
