dd57c0344ab23d1605155f09eaf297273e1272af
kimjuyi <juyi077@naver.com>
2024-03-26 09:42:26 +0900

    [BE] refactor : ToString 삭제 및 사용하지 않는 import 삭제

--- server/src/main/java/com/ssafy/ibalance/diet/dto/DietByDateDto.java
+++ server/src/main/java/com/ssafy/ibalance/diet/dto/DietByDateDto.java
@@ -1,19 +1,16 @@
 package com.ssafy.ibalance.diet.dto;
 
-import com.ssafy.ibalance.child.entity.Child;
 import com.ssafy.ibalance.diet.entity.Diet;
 import com.ssafy.ibalance.diet.entity.DietMenu;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
-@ToString
 public class DietByDateDto {
 
     private Diet diet;
