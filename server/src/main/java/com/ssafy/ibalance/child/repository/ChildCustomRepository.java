18242bb88cf21b1ac29136a538c1ed84a8971f11
kimjuyi <juyi077@naver.com>
2024-03-27 17:35:23 +0900

    [BE] feat : 마이페이지 자녀 상세 정보 조회 기능 구현

--- server/src/main/java/com/ssafy/ibalance/child/repository/ChildCustomRepository.java
+++ server/src/main/java/com/ssafy/ibalance/child/repository/ChildCustomRepository.java
@@ -1,9 +1,13 @@
 package com.ssafy.ibalance.child.repository;
 
+import com.ssafy.ibalance.child.dto.response.ChildDetailResponse;
+import com.ssafy.ibalance.member.entity.Member;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ChildCustomRepository {
 
     List<String> getMenuIdByChildIdAndDate(Integer childId, LocalDate today);
+    ChildDetailResponse getChildDetail(Integer childId, Member member);
 }
--- server/src/main/java/com/ssafy/ibalance/child/repository/ChildCustomRepositoryImpl.java
+++ server/src/main/java/com/ssafy/ibalance/child/repository/ChildCustomRepositoryImpl.java
@@ -1,12 +1,23 @@
 package com.ssafy.ibalance.child.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ibalance.child.dto.ChildDetailDto;
+import com.ssafy.ibalance.child.dto.response.ChildDetailResponse;
+import com.ssafy.ibalance.child.entity.Child;
+import com.ssafy.ibalance.child.entity.ChildAllergy;
+import com.ssafy.ibalance.child.exception.ChildAccessDeniedException;
+import com.ssafy.ibalance.child.exception.ChildNotFoundException;
+import com.ssafy.ibalance.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
 import static com.ssafy.ibalance.child.entity.QChild.child;
+import static com.ssafy.ibalance.child.entity.QChildAllergy.childAllergy;
 import static com.ssafy.ibalance.diet.entity.QDiet.diet;
 import static com.ssafy.ibalance.diet.entity.QDietMenu.dietMenu;
 
@@ -26,5 +37,32 @@ public class ChildCustomRepositoryImpl implements ChildCustomRepository {
                 .where(child.id.eq(childId))
                 .where(diet.dietDate.between(today.minusWeeks(2), today))
                 .fetch();
+    }
+
+    @Override
+    public ChildDetailResponse getChildDetail(Integer childId, Member member) {
+        Map<Child, List<ChildAllergy>> transform = jpaQueryFactory.select(child, childAllergy)
+                .from(child)
+                .leftJoin(childAllergy).on(child.id.eq(childAllergy.child.id))
+                .where(child.id.eq(childId))
+                .transform(
+                        groupBy(child).as(
+                                list(childAllergy)
+                        )
+                );
+
+        ChildDetailDto childDetailDto = transform.entrySet().stream()
+                .map(entry -> ChildDetailDto.builder()
+                        .child(entry.getKey())
+                        .allergies(entry.getValue())
+                        .build())
+                .findFirst()
+                .orElseThrow(() -> new ChildNotFoundException("해당하는 자녀가 없습니다."));
+
+        if(!member.equals(childDetailDto.getChild().getMember())) {
+            throw new ChildAccessDeniedException("조회 권한이 없습니다.");
+        }
+
+        return ChildDetailResponse.convertEntityToDto(childDetailDto);
     }
 }
