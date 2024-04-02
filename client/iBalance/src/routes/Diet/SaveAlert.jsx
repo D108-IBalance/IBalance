abe3a5397abc47ff05b466321ad8c37cf6bbe40c
suehwanBoo <rndjdieo119@naver.com>
2024-04-02 14:21:07 +0900

    [FE] refactor : SaveAlert CSS 변경

--- client/iBalance/src/routes/Diet/SaveAlert.jsx
+++ client/iBalance/src/routes/Diet/SaveAlert.jsx
@@ -4,9 +4,7 @@ const SaveAlert = () => {
   return (
     <>
       <div className={classes.back}>
-        <div className={classes.container}>
-          <h1>저장이 완료되었습니다.</h1>
-        </div>
+        <h1>Save Complete !</h1>
       </div>
     </>
   );
