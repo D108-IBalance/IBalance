19922ac1ac559badb72e5e7fc1f460dfda99267f
suehwanBoo <rndjdieo119@naver.com>
2024-04-03 14:41:15 +0900

    [FE] feat : 홈페이지 식단 여러개일때 렌더

--- client/iBalance/src/routes/Home/TodayDiet.jsx
+++ client/iBalance/src/routes/Home/TodayDiet.jsx
@@ -1,33 +1,52 @@
 import { useNavigate } from "react-router-dom";
 import classes from "./TodayDiet.module.css";
 import spoon from "../../assets/home/diet_icon1.png";
+import { useState } from "react";
 
 const TodayDiet = (props) => {
   const { userDiet } = props;
-
+  const [menuStep, setMenuStep] = useState(0);
+  const prevStep = () => {
+    setMenuStep((prev) => (prev === 0 ? userDiet.length - 1 : prev - 1));
+  };
+  const nextStep = () => {
+    setMenuStep((prev) => (prev === userDiet.length - 1 ? 0 : prev + 1));
+  };
   const navigate = useNavigate();
   return (
-    <section
-      className={classes.dietBox}
-      onClick={() => {
-        navigate("/diet");
-      }}>
+    <section className={classes.dietBox}>
       <div className={classes.dietTitle}>
-        <div className={classes.dietIcon} />
-        <span>오늘의 식단표</span>
+        <div style={{ display: "flex" }}>
+          <div className={classes.dietIcon} />
+          <span>오늘의 식단표</span>
+        </div>
+        {userDiet.length > 1 ? (
+          <div className={classes.direction}>
+            <div className={classes.prev} onClick={prevStep} />
+            <div className={classes.next} onClick={nextStep} />
+          </div>
+        ) : null}
       </div>
 
       {userDiet.length === 0 ? (
-        <div className={classes.noDietBox}>
+        <div
+          className={classes.noDietBox}
+          onClick={() => {
+            navigate("/diet");
+          }}>
           <div className={classes.emptyIcon} />
           <p>오늘의 식단을 추천받아보세요.</p>
         </div>
       ) : (
-        <div className={classes.todayDietBox}>
+        <div
+          className={classes.todayDietBox}
+          onClick={() => {
+            navigate("/diet");
+          }}>
           <img className={classes.spoonIcon} src={spoon} />
           <div className={classes.separate} />
           <div>
-            {userDiet[0].menuList.map((menu, idx) => {
+            {userDiet[menuStep].menuList.map((menu, idx) => {
               return (
                 <div key={idx} className={classes.menuText}>
                   {menu.menuName}
