dc1612b9a212c2ce68812c6d1b158bbaa260c5c3
suehwanBoo <rndjdieo119@naver.com>
2024-04-07 20:12:48 +0900

    [FE] fix : 아황산류 레시피 block

--- client/iBalance/src/server.jsx
+++ client/iBalance/src/server.jsx
@@ -1,4 +1,4 @@
-const servers = "http://localhost:5173";
-// const servers = "https://j10d108.p.ssafy.io";
+// const servers = "http://localhost:5173";
+const servers = "https://j10d108.p.ssafy.io";
 
 export default servers;
